5830457f7dbaafa3b9574b8fbdadd93268106061
Md.Shibly Rahman Alve <alverohman2000@gmail.com>
2022-08-07 11:17:08 +0600

    Fixed navigation between home and favourite page

--- src/ahiri/controllers/FavouriteController.java
+++ src/ahiri/controllers/FavouriteController.java
@@ -2,15 +2,28 @@
 package ahiri.controllers;
 
 import ahiri.Song;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 
 public class FavouriteController implements Initializable {
@@ -35,6 +48,49 @@ public class FavouriteController implements Initializable {
             new Song(3,"Amar Protichchobi","Biborton","Feb 15,2022","3:53","c","c")
     );
     
+    @FXML
+    private Button btnMore;
+    @FXML
+    private Button btnHome;
+    @FXML
+    private Button btnSearch;
+    @FXML
+    private Button btnLibrary;
+    @FXML
+    private Button btnCreatePlaylist;
+    @FXML
+    private Button btnFavourite;
+    @FXML
+    private ImageView selectedImg;
+    @FXML
+    private Label selectedSongName;
+    @FXML
+    private Label selectedArtist;
+    @FXML
+    private Label curTimeLabel;
+    @FXML
+    private Button previousButton;
+    @FXML
+    private ImageView imgPlayPause;
+    @FXML
+    private Button stopButton;
+    @FXML
+    private Button nextButton;
+    @FXML
+    private Button repeatButton;
+    @FXML
+    private Slider musicSlider;
+    @FXML
+    private Label endTimeLabel;
+    @FXML
+    private Button volumeButton;
+    @FXML
+    private Slider volumeSlider;
+    @FXML
+    private ComboBox speedBox;
+    
+    private String speeds[]={"0.25","0.50","0.75","1","1.25","1.5","1.75","2"};
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         colTitle.getStyleClass().add("bold");
@@ -46,6 +102,44 @@ public class FavouriteController implements Initializable {
         colDuration.setCellValueFactory(new PropertyValueFactory<Song,String>("duration"));
         
         tableViewFavourite.setItems(songs);
+        
+        for(int i=0;i<speeds.length;i++){
+                speedBox.getItems().add(speeds[i]);
+        }
     }    
+
+    @FXML
+    private void navigateHome(MouseEvent event)throws IOException {
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/Home.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    @FXML
+    private void previousMusic(ActionEvent event) {
+    }
+
+    @FXML
+    private void playMusic(ActionEvent event) {
+    }
+
+    @FXML
+    private void stopMusic(ActionEvent event) {
+    }
+
+    @FXML
+    private void nextMusic(ActionEvent event) {
+    }
+
+    @FXML
+    private void repeatMusic(ActionEvent event) {
+    }
+
+    @FXML
+    private void changeSpeed(ActionEvent event) {
+    }
     
 }
