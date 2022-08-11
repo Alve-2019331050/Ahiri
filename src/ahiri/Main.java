2b1e733c03e16664ac765061147daefd045cdae1
Farzana Reefat Raha <reefat.raha2018@gmail.com>
2022-08-11 14:45:18 +0600

    Minor change

--- src/ahiri/Main.java
+++ src/ahiri/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/Library.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/LoginSignup.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
